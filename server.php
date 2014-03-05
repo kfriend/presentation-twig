6d3bcb56340914d7079294f348a4ba3e643ea73d
Kevin Wood-Friend <kevin.wood.friend@gmail.com>
2014-03-05 13:29:48 -0500

    Add support for context file

--- server.php
+++ server.php
@@ -8,9 +8,14 @@ if (is_file(__DIR__."/templates/{$route}.twig"))
 {
 	// Load in Twig bootstrapper
 	require __DIR__.'/bootstrap.php';
-	// Parse template and return
 
-	echo $twig->render($route.'.twig');
+	// Get template context, if present
+	$context = is_file(__DIR__."/context/{$route}.php")
+		? include __DIR__."/context/{$route}.php"
+		: [];
+
+	// Parse template and return
+	echo $twig->render($route.'.twig', $context);
 	exit;
 }
 
