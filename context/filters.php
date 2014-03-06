618103e0c0fd7bd1283b683bce380a12729b2b01
Kevin Wood-Friend <kwoodfriend@rakacreative.com>
2014-03-06 14:56:34 -0500

    Filters example

---
+++ context/filters.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'copy' => <<<EOS
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+EOS
+    ,
+    'input' => "<a href='http://badguys.io'>I'M ATTEMPTING TO HACK YOU MUWAHAHAHAH!</a>",
+];
