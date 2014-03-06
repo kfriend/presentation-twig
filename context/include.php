0a33a477bdf55e9b397b5a31e6371a2ab313ffc9
Kevin Wood-Friend <kwoodfriend@rakacreative.com>
2014-03-06 15:26:29 -0500

    Include example

---
+++ context/include.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+    'languages' => [
+        [
+            'name' => 'Python',
+            'created' => 1991,
+        ],
+        [
+            'name' => 'PHP',
+            'created' => 1995,
+        ],
+        [
+            'name' => 'Ruby',
+            'created' => 1995,
+        ],
+        [
+            'name' => 'JavaScript',
+            'created' => 1995,
+        ],
+    ],
+];
